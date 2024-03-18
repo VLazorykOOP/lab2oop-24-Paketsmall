8d5ff19758e711c1cd1263acc3c4d6bed6f49514
Paketsmall <mehalp180@gmail.com>
2024-03-18 10:33:01 +0200

    лаб 2

--- OOPLab2Tpl/OOPLab2Tpl.cpp
+++ OOPLab2Tpl/OOPLab2Tpl.cpp
@@ -8,7 +8,7 @@ void Crypt(std::string text[4], unsigned short rez[4][16]) {
     // Запит на введення 4 рядків тексту від користувача
     for (int i = 0; i < 4; i++) {
         std::cout << "enter line " << i + 1 << ":";
-        std::getline(std::cin, text[i]);
+        std::getline(std::cin, text[i]); // Зчитування рядка з консолі
     }
 
     // Вивід введеного тексту
@@ -31,50 +31,50 @@ void Crypt(std::string text[4], unsigned short rez[4][16]) {
             unsigned char c;
             unsigned short r, t, b;
             r = 0;
-            c = text[i][j];
+            c = text[i][j]; // Отримання символу для шифрування з рядка
 
             // Встановлення частини результуючого числа за допомогою бітових операцій
             r |= i;
 
-            int asciiValue = static_cast<int>(c);
-            int higherPart = asciiValue >> 4;
-            int lowerPart = asciiValue & 0xF;
-            r |= (higherPart << 2);
+            int asciiValue = static_cast<int>(c); // Перетворення символу в ASCII-код
+            int higherPart = asciiValue >> 4; // Вибір вищої половини ASCII-коду
+            int lowerPart = asciiValue & 0xF; // Вибір нижньої половини ASCII-коду
+            r |= (higherPart << 2); // Встановлення вищої половини ASCII-коду в результуюче число
 
             t = 1;
             b = 0;
             for (l = 0; l < 5; l++) {
                 if (r & t) {
-                    if (b == 0) b = 1; else b = 0;
+                    if (b == 0) b = 1; else b = 0; // Визначення нового біта згідно зі специфікацією
                 }
-                t <<= 1;
+                t <<= 1; // Зсув бітової маски
             }
 
-            r |= b << 6;
+            r |= b << 6; // Встановлення біта у результуюче число
 
-            r |= (lowerPart << 7);
-            r |= (j << 11);
+            r |= (lowerPart << 7); // Встановлення нижньої половини ASCII-коду в результуюче число
+            r |= (j << 11); // Встановлення позиції символу в результуюче число
 
             t = 1;
             b = 0;
             for (l = 6; l < 16; l++) {
                 if (r & t) {
-                    if (b == 0) b = 1; else b = 0;
+                    if (b == 0) b = 1; else b = 0; // Визначення нового біта згідно зі специфікацією
                 }
-                t <<= 1;
+                t <<= 1; // Зсув бітової маски
             }
-            r |= b << 15;
+            r |= b << 15; // Встановлення біта у результуюче число
 
             rez[i][j] = r; // Збереження результату шифрування у масиві rez
             std::cout << std::hex << rez[i][j] << " "; // Вивід шифрованих даних
         }
-        std::cout << std::endl;
+        std::cout << std::endl; // Перехід на новий рядок після завершення обробки кожного рядка
     }
 }
 
 int main() {
     std::string text[4];
     unsigned short rez[4][16];
-    Crypt(text, rez);
+    Crypt(text, rez); // Виклик функції шифрування
     return 0;
 }
